24f8cd9446c0f68d3848cc2683d5d4b5707e0049
maddox earles <mdxear@gmail.com>
2023-05-11 21:21:45 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ['Guadalupe','Ollie','Aki'];
+const event = "surprise";
+
+ 
+ function writeCards( names, event ) {
+  let personalCards = []
+  for ( let i = 0; i < names.length; i++ ) {
+    personalCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+  }
+  return personalCards
+}
+
+function countDown() {
+    let countDown = 11
+    while (countDown > 0) {
+        countDown--
+        console.log(countDown)
+    }
+}
